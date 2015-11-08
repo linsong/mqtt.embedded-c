3a0076d8a28e8acaa27c3629573a77ab3e149ffa
Vincent Wang <vincent@linsong.org>
2015-11-08 23:03:03 +0800

    add a publisher example program

--- MQTTClient-C/samples/linux/build.sh
+++ MQTTClient-C/samples/linux/build.sh
@@ -1,3 +1,30 @@
 cp ../../src/MQTTClient.c .
 sed -e 's/""/"MQTTLinux.h"/g' ../../src/MQTTClient.h > MQTTClient.h
-gcc stdoutsub.c -I ../../src -I ../../src/linux -I ../../../MQTTPacket/src MQTTClient.c ../../src/linux/MQTTLinux.c ../../../MQTTPacket/src/MQTTFormat.c  ../../../MQTTPacket/src/MQTTPacket.c ../../../MQTTPacket/src/MQTTDeserializePublish.c ../../../MQTTPacket/src/MQTTConnectClient.c ../../../MQTTPacket/src/MQTTSubscribeClient.c ../../../MQTTPacket/src/MQTTSerializePublish.c -o stdoutsub ../../../MQTTPacket/src/MQTTConnectServer.c ../../../MQTTPacket/src/MQTTSubscribeServer.c ../../../MQTTPacket/src/MQTTUnsubscribeServer.c ../../../MQTTPacket/src/MQTTUnsubscribeClient.c
+
+arm-linux-gnueabi-gcc -I ../../src -I ../../src/linux -I ../../../MQTTPacket/src \
+-o stdoutsub stdoutsub.c MQTTClient.c \
+../../src/linux/MQTTLinux.c \
+../../../MQTTPacket/src/MQTTConnectClient.c \
+../../../MQTTPacket/src/MQTTConnectServer.c \
+../../../MQTTPacket/src/MQTTDeserializePublish.c \
+../../../MQTTPacket/src/MQTTFormat.c  \
+../../../MQTTPacket/src/MQTTPacket.c \
+../../../MQTTPacket/src/MQTTSerializePublish.c \
+../../../MQTTPacket/src/MQTTSubscribeClient.c \
+../../../MQTTPacket/src/MQTTSubscribeServer.c \
+../../../MQTTPacket/src/MQTTUnsubscribeClient.c \
+../../../MQTTPacket/src/MQTTUnsubscribeServer.c
+
+arm-linux-gnueabi-gcc -I ../../src -I ../../src/linux -I ../../../MQTTPacket/src \
+-o pub pub.c MQTTClient.c \
+../../src/linux/MQTTLinux.c \
+../../../MQTTPacket/src/MQTTConnectClient.c \
+../../../MQTTPacket/src/MQTTConnectServer.c \
+../../../MQTTPacket/src/MQTTDeserializePublish.c \
+../../../MQTTPacket/src/MQTTFormat.c  \
+../../../MQTTPacket/src/MQTTPacket.c  \
+../../../MQTTPacket/src/MQTTSerializePublish.c \
+../../../MQTTPacket/src/MQTTSubscribeClient.c \
+../../../MQTTPacket/src/MQTTSubscribeServer.c \
+../../../MQTTPacket/src/MQTTUnsubscribeClient.c \
+../../../MQTTPacket/src/MQTTUnsubscribeServer.c
